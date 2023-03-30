9c55dc06d37d76d732b6eebdae9f081b5a6b7103
lchase36 <chaselesh36@gmail.com>
2023-03-29 17:03:30 -0700

    Create tracking system for add/remove and style book visualization.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const myLibrary = [];
 const libraryElem = document.querySelector("#library");
 const addBookElem = document.querySelector("#add-book");
 const modal = document.querySelector("#add-book-modal");
 const overlay = document.querySelector("#overlay");
 const titleElem = document.querySelector("#title");
+const errorElem = document.querySelector(".duplicate-error");
 const authorElem = document.querySelector("#author");
 const pageCountElem = document.querySelector("#page-count");
 const isReadElem = document.querySelector("#read");
 const submit = document.querySelector("#submit");
+
+let myLibrary = [];
+let removeButtons = document.querySelectorAll(".prop-remove");
 
 function Book(title = "N/A", author = "N/A", pageCount = 0, isRead = false) {
   this.title = title;
@@ -20,6 +23,17 @@ Book.prototype.info = function bookInformation() {
   const readStr = this.isRead ? "has been read" : "not read yet";
   return `${this.title} by ${this.author}, ${this.pageCount} pages, ${readStr}`;
 };
+
+const removeBook = (e) => {
+  const divToRemove = e.target.parentElement;
+  const titleOfBook = divToRemove.querySelector(".prop-title");
+  const titleText = titleOfBook.textContent;
+  myLibrary = myLibrary.filter((book) => book.title !== titleText);
+  divToRemove.remove();
+};
+removeButtons.forEach((button) => {
+  button.addEventListener("click", removeBook);
+});
 
 const toggleModal = () => {
   modal.classList.toggle("hidden");
@@ -42,9 +56,11 @@ const visualizeBook = (book) => {
   bookRemove.textContent = "Remove";
 
   bookDiv.classList.add("book");
-  bookTitle.classList.add("book-property");
-  bookAuthor.classList.add("book-property");
-  bookPages.classList.add("book-property");
+  bookTitle.classList = "book-property prop-title";
+  bookAuthor.classList = "book-property prop-author";
+  bookPages.classList = "book-property prop-pages";
+  bookRead.classList = "book-button prop-read";
+  bookRemove.classList = "book-button prop-remove";
 
   bookDiv.appendChild(bookTitle);
   bookDiv.appendChild(bookAuthor);
@@ -54,21 +70,36 @@ const visualizeBook = (book) => {
   libraryElem.appendChild(bookDiv);
 };
 
+const rebuildVisual = () => {
+  libraryElem.innerHTML = "";
+  myLibrary.forEach(visualizeBook);
+  removeButtons = document.querySelectorAll(".prop-remove");
+  removeButtons.forEach((button) => {
+    button.addEventListener("click", removeBook);
+  });
+};
+
 const addBookToLibrary = (e) => {
   e.preventDefault();
-  const newBook = new Book();
-  newBook.title = titleElem.value;
-  newBook.author = authorElem.value;
-  newBook.pageCount = Number(pageCountElem.value);
-  newBook.isRead = isReadElem.checked;
-  myLibrary.push(newBook);
+  if (myLibrary.some((book) => book.title === titleElem.value)) {
+    errorElem.textContent = "This book is already in your library!";
+  } else {
+    errorElem.textContent = "";
+    const newBook = new Book();
+    newBook.title = titleElem.value;
+    newBook.author = authorElem.value;
+    newBook.pageCount = Number(pageCountElem.value);
+    newBook.isRead = isReadElem.checked;
+    myLibrary.push(newBook);
 
-  titleElem.value = "";
-  authorElem.value = "";
-  pageCountElem.value = "";
-  isReadElem.checked = false;
+    titleElem.value = "";
+    authorElem.value = "";
+    pageCountElem.value = "";
+    isReadElem.checked = false;
 
-  myLibrary.forEach(visualizeBook);
+    toggleModal();
+    rebuildVisual();
+  }
 };
 
 addBookElem.addEventListener("click", toggleModal);
